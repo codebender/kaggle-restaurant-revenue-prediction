3aad97bc96f8f2b86f28c0d0d91dcdcc9d4395e0
Matthew Bender <benderm@gmail.com>
2015-03-25 00:16:54 -0600

    add back Type, set MB to NA
    score: 1778056.35073

--- conditional_random_forest.R
+++ conditional_random_forest.R
@@ -15,13 +15,12 @@ train$City.Group <- NULL
 test$City <- NULL
 test$Open.Date <- NULL
 test$City.Group <- NULL
-train$Type <- NULL
-test$Type <- NULL
 
-#train$Type = as.factor(train$Type)
-#test$Type = as.factor(test$Type)
+train$Type = as.factor(train$Type)
+test$Type[test$Type == "MB"] <- NA
+test$Type = as.factor(test$Type)
 
-rf = cforest(revenue ~., data = train[,-1], controls=cforest_unbiased(ntree=2000, mtry=3))
+rf = cforest(revenue ~., data = train[,-1], controls=cforest_unbiased(ntree=4000))
 
 Prediction = predict(rf, test[,-1], OOB=TRUE, type = "response")
 
@@ -29,4 +28,4 @@ id<-test[,1]
 submission<-cbind(id,Prediction)
 colnames(submission)[2] <- "Prediction"
 
-write.csv(submission, "Output/conditional_forest_ntree_2000.csv", row.names = FALSE, quote = FALSE)
+write.csv(submission, "Output/conditional_forest_mb_set_to_na_ntree_2000.csv", row.names = FALSE, quote = FALSE)
