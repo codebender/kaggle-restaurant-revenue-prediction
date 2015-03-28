b41f9036e3a7078215bf6fedf1437b588acf4c28
Matthew Bender <benderm@gmail.com>
2015-03-28 17:40:53 -0600

    remove Type and city_group
    score: 1735548.28648

--- conditional_random_forest.R
+++ conditional_random_forest.R
@@ -5,26 +5,22 @@ library(party)
 train = read.csv("Data/train.csv", header = TRUE, stringsAsFactors = FALSE)
 test = read.csv("Data/test.csv", header = TRUE, stringsAsFactors = FALSE)
 
-# make city group string factors
-train$city_group = as.factor(train$City.Group)
-test$city_group = as.factor(test$City.Group)
-
 # parse days open from start of competetion
 competition_start <- strptime('23.03.2015', format='%d.%m.%Y')
-train$days_open <- as.numeric(difftime(competition_start, strptime(train$Open.Date, format='%m/%d/%Y'), units='days'))
-test$days_open <- as.numeric(difftime(competition_start, strptime(test$Open.Date, format='%m/%d/%Y'), units='days'))
+train$days_open <- as.numeric(difftime(competition_start,
+  strptime(train$Open.Date, format='%m/%d/%Y'), units='days'))
+test$days_open <- as.numeric(difftime(competition_start,
+  strptime(test$Open.Date, format='%m/%d/%Y'), units='days'))
 
-# remove unused columns
+# remove unneeded columns
 train$City <- NULL
 train$Open.Date <- NULL
 train$City.Group <- NULL
+train$Type <- NULL
 test$City <- NULL
 test$Open.Date <- NULL
 test$City.Group <- NULL
-
-train$Type = as.factor(train$Type)
-test$Type[test$Type == "MB"] <- NA
-test$Type = as.factor(test$Type)
+test$Type <- NULL
 
 rf = cforest(revenue ~., data = train[,-1], controls=cforest_unbiased(ntree=2000))
 
@@ -34,4 +30,4 @@ id<-test[,1]
 submission<-cbind(id,Prediction)
 colnames(submission)[2] <- "Prediction"
 
-write.csv(submission, "Output/conditional_forest_days_open_ntree_2000.csv", row.names = FALSE, quote = FALSE)
+write.csv(submission, "Output/conditional_forest_days_open_log_no_city_group_or_type_ntree_2000.csv", row.names = FALSE, quote = FALSE)
