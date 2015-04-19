e1a8542f87466fcc07576b027238335c0c2e19e6
Matthew Bender <benderm@gmail.com>
2015-04-19 13:01:01 -0600

    beat the benchmark code from forum
    score: 1758998.75438

---
+++ Beat_The_Benchmark (1).R
@@ -0,0 +1,47 @@
+library(Boruta)
+library(caret)
+
+train <- read.csv("data/train.csv")
+test  <- read.csv("data/test.csv")
+
+n.train <- nrow(train)
+
+test$revenue <- 1
+myData <- rbind(train, test)
+rm(train, test)
+
+#Tranform Time
+myData$Open.Date <- as.POSIXlt("01/01/2015", format="%m/%d/%Y") - as.POSIXlt(myData$Open.Date, format="%m/%d/%Y")
+myData$Open.Date <- as.numeric(myData$Open.Date / 1000) #Scale for factors
+
+#Consolidate Cities
+myData$City                                      <- as.character(myData$City)
+myData$City[myData$City.Group == "Other"]        <- "Other"
+myData$City[myData$City == unique(myData$City)[4]] <- unique(myData$City)[2]
+myData$City                                      <- as.factor(myData$City)
+myData$City.Group                                <- NULL
+
+#Consolidate Types
+myData$Type <- as.character(myData$Type)
+myData$Type[myData$Type=="DT"] <- "IL"
+myData$Type[myData$Type=="MB"] <- "FC"
+myData$Type <- as.factor(myData$Type)
+
+#Log Transform P Variables and Revenue
+myData[, paste("P", 1:37, sep="")] <- log(1 +myData[, paste("P", 1:37, sep="")])
+
+myData$revenue <- log(myData$revenue)
+
+important <- Boruta(revenue~., data=myData[1:n.train, ])
+
+#Random Forest
+model <- train(revenue~., 
+               data=myData[1:n.train, c(important$finalDecision != "Rejected", TRUE)])
+
+#Make a Prediction
+prediction <- predict(model, myData[-c(1:n.train), ])
+
+#Make Submission
+submit<-as.data.frame(cbind(seq(0, length(prediction) - 1, by=1), exp(prediction)))
+colnames(submit)<-c("Id","Prediction")
+write.csv(submit,"submission.csv",row.names=FALSE,quote=FALSE)
