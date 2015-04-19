496f4371807717ec1f3f5930462d924fb1ada788
Matthew Bender <benderm@gmail.com>
2015-04-19 13:27:40 -0600

    new base for improvement

--- conditional_random_forest.R
+++ conditional_random_forest.R
@@ -22,15 +22,16 @@ train$months_open <- as.numeric(difftime(competition_start,
 test$months_open <- as.numeric(difftime(competition_start,
   strptime(test$Open.Date, format='%m/%d/%Y'), units='days')/30)
 
+
 # remove unneeded columns
-train$City <- NULL
 train$Open.Date <- NULL
-train$City.Group <- NULL
 train$Type <- NULL
-test$City <- NULL
-test$Open.Date <- NULL
-test$City.Group <- NULL
+train$City <- NULL
+train$City.Group <- NULL
 test$Type <- NULL
+test$City <- NULL
+test$City.Group <- NULL
+test$Open.Date <- NULL
 
 rf = cforest(revenue ~., data = train[,-1], controls=cforest_unbiased(ntree=2000))
 
@@ -40,4 +41,4 @@ id<-test[,1]
 submission<-cbind(id,Prediction)
 colnames(submission)[2] <- "Prediction"
 
-write.csv(submission, "Output/conditional_forest_days_weeks_months_open_ntree_2000.csv", row.names = FALSE, quote = FALSE)
+write.csv(submission, "Output/conditional_forest.csv", row.names = FALSE, quote = FALSE)
